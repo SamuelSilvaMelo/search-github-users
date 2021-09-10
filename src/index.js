a87f6a25d397c999a53fd8d574a3d26f742a2fe1
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-09-10 17:41:30 -0300

    Add basename route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter>
+    <BrowserRouter basename="/search-github-users">
       <App />
     </BrowserRouter>
   </Provider>,
